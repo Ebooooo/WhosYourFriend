782f791b84dc3a5a57053cb0e88b8ecee8997c60
Ebooooo <maticover2@gmail.com>
2022-06-05 16:10:00 +0200

    Talk_action
    Talk action added

---
+++ Assets/Scripts/ActionKey.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionKey : MonoBehaviour
+{
+    public GameObject talk;
+    private PlayerController talk_player;
+    void Start()
+    {
+        talk_player = GameObject.Find("Player").GetComponent<PlayerController>();
+        talk.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TalkUse();
+    }
+    private void OnTriggerEnter2D(Collider2D col) 
+    {
+    if(col.CompareTag("Player"))
+    {
+        Debug.Log("kolizja");
+        talk.SetActive(true);
+    }
+    }
+    private void TalkUse()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && talk.active == true)
+        {
+            Debug.Log("Talk_usage");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        talk.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController: MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 5f;
+    private float horizontal;
+    private float speedLimit = 0.7f;
+
+    private Transform body;
+    void Start()
+    {
+        body = transform;
+    }
+
+    void Update()
+    {
+        InputCollection();
+        Movement();
+    }
+
+    private void InputCollection()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void Movement()
+    {
+        float horizontalSpeed = horizontal * moveSpeed;
+        if(horizontal != 0)
+        {
+            body.position += new Vector3(horizontalSpeed * Time.deltaTime, 0f, 0f);
+        }
+    }
+}
